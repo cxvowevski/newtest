30e3d92cdecf3761038fce01200e7b8b21d15ba9
cxvowevski <62030509+cxvowevski@users.noreply.github.com>
2020-04-08 03:33:13 +0800

    iou
    oiu

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Gerbert;
+
+public class Main{
+    public static void main(String [] args){
+        int a, b, sum;
+        a=100;
+        b=200;
+        sum=a*b;
+        System.out.println(sum);
+        int c=125;
+        if(c>=a){
+            System.out.println(c*a*b);
+        }
+    }
+}
